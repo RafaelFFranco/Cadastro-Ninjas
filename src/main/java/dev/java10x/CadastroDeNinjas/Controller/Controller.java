ec69201d7549851fd2c08014015d1c771eb7b2d9
Rafael Franco <rafaelfariafranco30@gmail.com>
2025-01-14 21:56:26 -0300

    Controller criada [Aula 4]

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeNinjas.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //declarar que é um controller
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/BemVindo") //Ação de Get
+    public String boasVindas(){
+        return "Boas Vindas";
+    }
+
+}
