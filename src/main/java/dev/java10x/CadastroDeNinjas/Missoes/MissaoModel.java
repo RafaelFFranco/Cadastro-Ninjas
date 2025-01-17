f58fe1c80b55d947ecd17f5c5aa3684b6a3f76d2
Rafael Franco <rafaelfariafranco30@gmail.com>
2025-01-16 21:39:24 -0300

    Lombok Anotations [Aula 9]

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninja.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MissaoModel {
 
     @Id
@@ -20,36 +26,4 @@ public class MissaoModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissaoModel() {
-    }
-
-    public MissaoModel(Long id, RankMissaoEnum rank, String descricao) {
-        this.id = id;
-        this.rank = rank;
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public RankMissaoEnum getRank() {
-        return rank;
-    }
-
-    public void setRank(RankMissaoEnum rank) {
-        this.rank = rank;
-    }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -2,10 +2,16 @@ package dev.java10x.CadastroDeNinjas.Ninja;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //JPA = java persistence API
 @Entity // transforma uma classe em uma entidade do banco de dados
 @Table(name = "tb_ninjas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -20,36 +26,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // chave estrangeira da missao, (pk da missão)
     private MissaoModel missoes;
 
-
-    public NinjaModel() {}
-
-    public NinjaModel(String aldeia, int idade, String nome) {
-        this.aldeia = aldeia;
-        this.idade = idade;
-        this.nome = nome;
-    }
-
-    public String getAldeia() {
-        return aldeia;
-    }
-
-    public void setAldeia(String aldeia) {
-        this.aldeia = aldeia;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
